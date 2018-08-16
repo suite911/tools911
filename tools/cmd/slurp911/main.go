b0ff82848bc432c322d392bd763f18f5bf8232c2
Amy <amyadzuki@gmail.com>
2018-08-16 21:48:50 +0000

    Update main.go

--- tools/cmd/slurp911/main.go
+++ tools/cmd/slurp911/main.go
@@ -12,15 +12,15 @@ import (
 
 func main() {
 	if len(os.Args) < 4 {
-		fmt.Fprintln(usage)
+		fmt.Fprintln(os.Stderr, usage)
 		if len(os.Args) >= 2 {
 			os.Exit(1)
 		}
 		return
 	}
 
-	if err := slurp911.Main(os.Args[0], os.Args[3:], os.Args[1], os.Args[2]); err != nil {
-		fmt.Fprintln(usage)
+	if err := slurp911.Main(os.Args[0], os.Args[1], os.Args[4:], os.Args[2], os.Args[3]); err != nil {
+		fmt.Fprintln(os.Stderr, usage)
 		log.Fatalln(err)
 	}
 }
@@ -28,7 +28,8 @@ func main() {
 var usage string
 
 func init() {
-	usage = "usage: " + os.Args[0] + " " + vt.U("PKGNAME") + " " + vt.U("VARNAME")  " " +
+	usage = "usage: " + os.Args[0] + " " + vt.U("OUTPATH") + " " +
+		vt.U("PKGNAME") + " " + vt.U("VARNAME")  " " +
 		vt.U("KEY") + ":" + vt.U("PATH") + " " + vt.S("[") +
 		vt.U("KEY") + ":" + vt.U("PATH") + " " + vt.S("[...]]")
 }
